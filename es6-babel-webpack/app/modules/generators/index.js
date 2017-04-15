56f497c902bf79887fda9fb3574349bb0a63ca87
Arvind <europa1613@users.noreply.github.com>
2017-04-15 19:58:02 -0400

    Loop Number Generator

--- es6-babel-webpack/app/modules/generators/index.js
+++ es6-babel-webpack/app/modules/generators/index.js
@@ -1,28 +1,47 @@
 import { log, logTitle } from '../HTMLLogger';
 
 export default () => {
-	logTitle('ES6 Generators');
+    logTitle('ES6 Generators');
 
-	const generateValues = function* () {
-		yield 1;
-		yield 2;
-		yield true;
-		yield null;
-		yield {
-			name: 'John Snow'
-		};
+    const generateValues = function*() {
+        yield 1;
+        yield 2;
+        yield true;
+        yield null;
+        yield {
+            name: 'John Snow'
+        };
 
-		return {
-			abc: ' Generator done!'
-		}
-	}
+        return {
+            abc: ' Generator done!'
+        }
+    }
 
-	const valueGenerator = generateValues(); 
+    const valueGenerator = generateValues();
 
-	log(JSON.stringify(valueGenerator.next().value));
-	log(JSON.stringify(valueGenerator.next().value));
-	log(JSON.stringify(valueGenerator.next().value));
-	log(JSON.stringify(valueGenerator.next().value));
-	log(JSON.stringify(valueGenerator.next().value));
-	log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+    log(JSON.stringify(valueGenerator.next().value));
+
+
+    const loopNumbers = function*(numbers) {
+        for (let i = 0; i < numbers.length; i++) {
+            yield numbers[i];
+        }
+    };
+
+    const loopNumbersGen = loopNumbers([12,22,33,44,55,66,99]);
+
+    const interval = setInterval(() => {
+    	const next = loopNumbersGen.next();
+    	if(next.done) {
+    		log('Loop Numbers done!');
+    		clearInterval(interval);
+    	} else {
+    		log(next.value);
+    	}
+    }, 1000);
 }
